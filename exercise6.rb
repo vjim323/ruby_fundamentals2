cf9c20038d8db14b45c7d22cf23e46922a3b6e09
Vincent Jim <vjim323@gmail.com>
2014-10-22 15:45:47 -0400

    Added feature of checking for bananas

--- exercise6.rb
+++ exercise6.rb
@@ -14,3 +14,9 @@ grocery_list << "rice"
 display_groceries(grocery_list)
 
 puts grocery_list.length
+
+if grocery_list.include?("bananas") 
+	puts "You need to pick up bananas."
+else 
+	puts "You don't need to pick up bananas today."
+end
