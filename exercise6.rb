3fe2469b855d5ec02ae91e102747c05b851f0dda
Vincent Jim <vjim323@gmail.com>
2014-10-22 15:48:39 -0400

    Displays second item on the grocery list

--- exercise6.rb
+++ exercise6.rb
@@ -7,16 +7,19 @@ def display_groceries(list)
 	end
 end
 
-display_groceries(grocery_list)
+# display_groceries(grocery_list)
 
-grocery_list << "rice"
+# grocery_list << "rice"
 
-display_groceries(grocery_list)
+# display_groceries(grocery_list)
 
-puts grocery_list.length
+# puts grocery_list.length
 
-if grocery_list.include?("bananas") 
-	puts "You need to pick up bananas."
-else 
-	puts "You don't need to pick up bananas today."
-end
+# if grocery_list.include?("bananas") 
+# 	puts "You need to pick up bananas."
+# else 
+# 	puts "You don't need to pick up bananas today."
+# end
+
+puts grocery_list[1]
+
