3375f37b4db96b3d425f568b2ca0827a7210802d
Vincent Jim <vjim323@gmail.com>
2014-10-22 15:39:50 -0400

    Displays the old list but also adds rice to the list and redisplays

--- exercise6.rb
+++ exercise6.rb
@@ -1,7 +1,14 @@
 #Assignment 6 - grocery list
-
 grocery_list = ["carrots", "broccoli", "spinach", "toilet paper"]
 
-grocery_list.each do |grocery_item|
-	puts "* " + grocery_item
+def display_groceries(list)
+	list.each do |grocery_item|
+		puts "* " + grocery_item
+	end
 end
+
+display_groceries(grocery_list)
+
+grocery_list << "rice"
+
+display_groceries(grocery_list)
